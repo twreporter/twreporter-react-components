20678f70fe3dfa8416a536c008752b50bd587dec
yucj <nakts0123@gmail.com>
2017-09-01 15:21:20 +0800

    Update export

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 import Footer from './footer-components/src'
 import Header from './header-components/src'
 import IndexPageComposite from './index-components/src'
-import TopicsComposite from './listing-components/src'
+import ListingComposite from './listing-components/src'
 import FetchingWrapper from './shared/components/is-fetching-wrapper'
+
+const { TopicsList, List } = ListingComposite
+
+const Wrapper = {
+  FetchingWrapper,
+}
 
 export default {
   Footer,
   Header,
   IndexPageComposite,
-  TopicsComposite,
-  Wrapper: {
-    FetchingWrapper,
-  },
+  TopicsList,
+  List,
+  Wrapper,
+}
+
+export {
+  Footer,
+  Header,
+  IndexPageComposite,
+  TopicsList,
+  List,
+  Wrapper,
 }
